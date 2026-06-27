353e3289c4ccc05c35fcb4b8d6d49bddb546a1c4
Dan <mcgeedan@berkeley.edu>
2026-06-27 15:40:12 -0700

    pass HOME to sudo in nixswitch/nixtest so --impure secrets path resolves correctly
    sudo resets HOME to /root, causing builtins.getEnv HOME to return the
    wrong path. Passing HOME=$HOME explicitly preserves the user's home
    directory without needing sudo -E or sudoers changes.

--- modules/home-manager/term/zsh.nix
+++ modules/home-manager/term/zsh.nix
@@ -52,9 +52,9 @@
 
       #Bens
 
-      nixswitch = "st=\"$(date +%s)\"; sudo nixos-rebuild switch --flake $HOME/nixos/#XiaNix --cores 8 --impure && notify-send 'updated' \"Took: $(($(date +%s)-$st))s\"";
+      nixswitch = "st=\"$(date +%s)\"; sudo HOME=$HOME nixos-rebuild switch --flake $HOME/nixos/#XiaNix --cores 8 --impure && notify-send 'updated' \"Took: $(($(date +%s)-$st))s\"";
       homeswitch = "st=\"$(date +%s)\"; home-manager switch --flake $HOME/nixos/#XiaNix --cores 8 --impure && notify-send 'updated' \"Took: $(($(date +%s)-$st))s\"";
-      nixtest = "st=\"$(date +%s)\"; sudo nixos-rebuild test --fast --flake $HOME/nixos/#XiaNix --cores 8 --impure && notify-send 'updated' \"Took: $(($(date +%s)-$st))s\"";
+      nixtest = "st=\"$(date +%s)\"; sudo HOME=$HOME nixos-rebuild test --fast --flake $HOME/nixos/#XiaNix --cores 8 --impure && notify-send 'updated' \"Took: $(($(date +%s)-$st))s\"";
       nixwatch = "cd ~/nixos && dirwatch nixtest";
       homewatch = "cd ~/nixos && dirwatch homeswitch";
       powerinfo = "upower -i /org/freedesktop/UPower/devices/battery_BAT1";
