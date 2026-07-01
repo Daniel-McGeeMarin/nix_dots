b37934e7a956c5e4d4a6f8c640b04e9c29c2a91f
Dan <mcgeedan@berkeley.edu>
2026-07-01 03:44:07 -0700

    remove fileSystems ZFS entry, let ZFS auto-mount via extraPools

--- hosts/XiaServer/storage.nix
+++ hosts/XiaServer/storage.nix
@@ -4,11 +4,6 @@
   boot.zfs.extraPools = [ "data" ];
   boot.zfs.requestEncryptionCredentials = true;
 
-  fileSystems."/srv" = {
-    device = "data/srv";
-    fsType = "zfs";
-  };
-
   systemd.tmpfiles.rules = [
     "d /srv/data              0755 root root"
     "d /srv/data/authelia     0750 root root"
