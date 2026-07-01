367e2f61c94eb26bbc40cefa8d02b1c51a9a3012
Dan <mcgeedan@berkeley.edu>
2026-07-01 03:38:18 -0700

    declare ZFS pool and srv mounts in NixOS config

--- hosts/XiaServer/storage.nix
+++ hosts/XiaServer/storage.nix
@@ -1,5 +1,20 @@
 { ... }:
 {
   boot.supportedFilesystems = [ "zfs" ];
-  boot.zfs.requestEncryptionCredentials = false;
+  boot.zfs.extraPools = [ "data" ];
+  boot.zfs.requestEncryptionCredentials = true;
+
+  fileSystems."/srv" = {
+    device = "data/srv";
+    fsType = "zfs";
+  };
+
+  systemd.tmpfiles.rules = [
+    "d /srv/data              0755 root root"
+    "d /srv/data/authelia     0750 root root"
+    "d /srv/data/homarr       0750 root root"
+    "d /srv/data/ghost-public 0750 root root"
+    "d /srv/data/ghost-private 0750 root root"
+    "d /srv/www               0755 root root"
+  ];
 }
