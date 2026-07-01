67232ea3a2a66d001bce1b68f57fa2f7d69d0776
Dan <mcgeedan@berkeley.edu>
2026-07-01 00:34:03 -0700

    disable GDM auto-suspend on server

--- system/serv/default.nix
+++ system/serv/default.nix
@@ -18,6 +18,8 @@
     systemd.targets.hibernate.enable = false;
     systemd.targets.hybrid-sleep.enable = false;
 
+    services.displayManager.gdm.autoSuspend = false;
+
     services.logind = {
       lidSwitch = "ignore";
       lidSwitchExternalPower = "ignore";
