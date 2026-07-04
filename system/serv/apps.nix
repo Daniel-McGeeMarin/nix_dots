4ce103922cba86e879fa46712ce0086c0f6791f9
Dan <mcgeedan@berkeley.edu>
2026-07-04 03:29:38 -0700

    apps: two containers (web + api), route /api/* to FastAPI

--- system/serv/apps.nix
+++ system/serv/apps.nix
@@ -3,22 +3,33 @@
   options.serv.apps.site.enable = lib.mkEnableOption "personal site";
 
   config = lib.mkIf config.serv.apps.site.enable {
-    virtualisation.oci-containers.containers.site = {
-      image  = "ghcr.io/daniel-mcgeemarin/mcgeeinfov2:latest";
-      ports  = [ "127.0.0.1:3001:80" ];
-      labels."io.containers.autoupdate" = "registry";
+    virtualisation.oci-containers.containers = {
+      site-web = {
+        image  = "ghcr.io/daniel-mcgeemarin/mcgeeinfov2-web:latest";
+        ports  = [ "127.0.0.1:3001:80" ];
+        labels."io.containers.autoupdate" = "registry";
+      };
+      site-api = {
+        image  = "ghcr.io/daniel-mcgeemarin/mcgeeinfov2-api:latest";
+        ports  = [ "127.0.0.1:8000:8000" ];
+        labels."io.containers.autoupdate" = "registry";
+      };
     };
 
     services.caddy.virtualHosts = {
       "http://mcgeedan.com".extraConfig = ''
-        reverse_proxy 127.0.0.1:3001
+        handle /api/* {
+          reverse_proxy 127.0.0.1:8000
+        }
+        handle {
+          reverse_proxy 127.0.0.1:3001
+        }
       '';
       "http://www.mcgeedan.com".extraConfig = ''
         redir http://mcgeedan.com{uri} permanent
       '';
     };
 
-    # Pull updated container images every 5 minutes
     systemd.timers.podman-auto-update = {
       wantedBy = [ "timers.target" ];
       timerConfig = {
