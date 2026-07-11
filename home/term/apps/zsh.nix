fcef51ea5c7b4c469698a8c0f6b7b70bb9011d8d
Dan <mcgeedan@berkeley.edu>
2026-07-11 02:42:08 -0700

    timeout notify-send in nixswitch to prevent server hang

--- home/term/apps/zsh.nix
+++ home/term/apps/zsh.nix
@@ -53,7 +53,7 @@ pys = "source ./venv/bin/activate";
 
       #Bens
 
-      nixswitch = "st=\"$(date +%s)\"; sudo HOME=$HOME nixos-rebuild switch --flake $HOME/nixos/#${flakeAttr} --cores 8 --impure && notify-send 'updated' \"Took: $(($(date +%s)-$st))s\"";
+      nixswitch = "st=\"$(date +%s)\"; sudo HOME=$HOME nixos-rebuild switch --flake $HOME/nixos/#${flakeAttr} --cores 8 --impure && timeout 5 notify-send 'updated' \"Took: $(($(date +%s)-$st))s\"";
       homeswitch = "st=\"$(date +%s)\"; home-manager switch --flake $HOME/nixos/#${flakeAttr} --cores 8 --impure && notify-send 'updated' \"Took: $(($(date +%s)-$st))s\"";
       nixtest = "st=\"$(date +%s)\"; sudo HOME=$HOME nixos-rebuild test --fast --flake $HOME/nixos/#${flakeAttr} --cores 8 --impure && notify-send 'updated' \"Took: $(($(date +%s)-$st))s\"";
       nixwatch = "cd ~/nixos && dirwatch nixtest";
