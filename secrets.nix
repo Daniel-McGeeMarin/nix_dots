dae66854f893ab73d39db0c4cc4e33f00e49bf1b
Dan <mcgeedan@berkeley.edu>
2026-07-10 23:10:35 -0700

    Add XiaServer user SSH key as agenix recipient

--- secrets.nix
+++ secrets.nix
@@ -1,14 +1,15 @@
 let
-  xiaserver = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINWFmoYiRIbUToYku4tbARtl7W0OLx+lSt2cwV0iSaj1";
+  xiaserver     = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINWFmoYiRIbUToYku4tbARtl7W0OLx+lSt2cwV0iSaj1";
+  xiaserverUser = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDbmhHJ1xWI+bq3WB9cdjuingSb/XJnSIvFcXZgPGHhU XiaServer@XiaServer";
 in {
-  "secrets/cloudflare-tunnel.age".publicKeys    = [ xiaserver ];
-  "secrets/authelia-jwt.age".publicKeys        = [ xiaserver ];
-  "secrets/authelia-session.age".publicKeys    = [ xiaserver ];
-  "secrets/authelia-storage.age".publicKeys    = [ xiaserver ];
-  "secrets/homarr-env.age".publicKeys           = [ xiaserver ];
-  "secrets/ghost-public-env.age".publicKeys     = [ xiaserver ];
-  "secrets/ghost-private-env.age".publicKeys    = [ xiaserver ];
-  "secrets/ocis-admin-password.age".publicKeys  = [ xiaserver ];
-  "secrets/graphide-api-env.age".publicKeys     = [ xiaserver ];
-  "secrets/ghcr-token.age".publicKeys           = [ xiaserver ];
+  "secrets/cloudflare-tunnel.age".publicKeys    = [ xiaserver xiaserverUser ];
+  "secrets/authelia-jwt.age".publicKeys        = [ xiaserver xiaserverUser ];
+  "secrets/authelia-session.age".publicKeys    = [ xiaserver xiaserverUser ];
+  "secrets/authelia-storage.age".publicKeys    = [ xiaserver xiaserverUser ];
+  "secrets/homarr-env.age".publicKeys           = [ xiaserver xiaserverUser ];
+  "secrets/ghost-public-env.age".publicKeys     = [ xiaserver xiaserverUser ];
+  "secrets/ghost-private-env.age".publicKeys    = [ xiaserver xiaserverUser ];
+  "secrets/ocis-admin-password.age".publicKeys  = [ xiaserver xiaserverUser ];
+  "secrets/graphide-api-env.age".publicKeys     = [ xiaserver xiaserverUser ];
+  "secrets/ghcr-token.age".publicKeys           = [ xiaserver xiaserverUser ];
 }
